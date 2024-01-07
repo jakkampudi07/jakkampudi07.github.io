aac117986f175c8aa302a1c8e05bbef3798d5396
JAKKAMPUDI JASWANTH KRISHNA <22Q95A6713@mrce.in>
2024-01-07 16:36:27 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const container = document.getElementById('container');
-  const audio = document.getElementById('birthdayAudio');
-
-  container.addEventListener('mouseenter', function() {
-    audio.play();
-  });
-});
-
